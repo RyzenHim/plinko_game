d519f9e4066c46c09ec0445fe9ccdec50e747296
Himanshu <himanshuupadhyay210@gmail.com>
2026-06-07 07:14:48 +0530

    backend deployed upadate in frontend

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
+const API_URL =
+  "https://plinko-game-j0ej.onrender.com" || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: API_URL,
