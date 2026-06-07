237ec389ff6bd87a2e95c9f758c1695058107562
Himanshu <himanshuupadhyay210@gmail.com>
2026-06-07 06:44:59 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 const roundRoutes = require("./src/routes/rounds.routes");
